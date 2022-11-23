7802790e94f997e50fded0d7efb99984b349bae7
Wellington Feijó <wellingtonfc95@gmail.com>
2022-11-23 00:11:58 -0300

    Implementação Delete Ususário

--- src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -5,8 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -57,5 +63,23 @@ public class GreetingsController {
 		
 		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);/*Retorna a lista em JSON*/
 	}
+	
+	@PostMapping(value = "salvar") //Mapeia a URL
+	@ResponseBody//Descrição da Resposta
+	public ResponseEntity<Usuario> salvar (@RequestBody Usuario usuario){/*Recebe os dados para salvar*/
+		
+		Usuario user = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
+	}
    
+	@DeleteMapping(value = "delete")/*mapeia a url*/
+	@ResponseBody /*Descrição da resposta*/
+	public ResponseEntity<String> delete(@RequestParam Long iduser){/*Recebe os dados para deletar*/
+		
+		usuarioRepository.deleteById(iduser);
+		
+		return new ResponseEntity<String>("Usuário deletado com sucesso!!", HttpStatus.OK);
+		
+	}
 }
